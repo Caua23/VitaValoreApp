d431a11ed2a3e871adbfa1bb81b7001f86576030
Vargas <77256510+Caua23@users.noreply.github.com>
2024-09-17 23:43:40 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:vita_valore/agua.dart';
+import 'package:vita_valore/login_sign.dart';
 
 void main() {
   runApp(const MaterialApp(
     title: 'Vita Valore',
     debugShowCheckedModeBanner: false,
-    home: AguaPage(),
+    home: LoginSing(),
   ));
 }
