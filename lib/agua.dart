a8353584a66dcac127fca019e62b1ea62ae20d71
Vargas <cauavargas1849@gmail.com>
2024-09-18 14:15:37 -0300

    fixing bug

--- lib/agua.dart
+++ lib/agua.dart
@@ -1,5 +1,3 @@
-//Nome da branch: VitaValoreAgua
-
 import 'package:flutter/material.dart';
 
 class AguaPage extends StatelessWidget {
@@ -9,201 +7,128 @@ class AguaPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: const Text("Hora de beber água!"),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Stack(
-              children: [
-                const Spacer(),
-                Transform.translate(
-                  offset: const Offset(90, -20),
-                  child: Image.asset(
-                    'assets/Ze_gotinha_da_agua.png',
-                    height: 140,
-                  ),
-                ),
-                Transform.translate(
-                  offset: const Offset(-90, -10),
-                  child: Image.asset(
-                    'assets/VitaValoreLogo.jpeg',
-                    height: 60,
-                  ),
-                ),
-                Transform.translate(
-                  offset: const Offset(105, 45),
-                  child: const Text(
-                    "1h26min",
-                    style: TextStyle(
-                      fontFamily: 'assets/fonts/GlacialIndifference',
-                      fontSize: 15,
-                      color: Color.fromARGB(255, 132, 0, 255),
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Transform.translate(
-                  offset: const Offset(-35, 190),
-                  child: const Text(
-                    "800ml",
-                    style: TextStyle(
-                        fontFamily: 'assets/fonts/GlacialIndifference',
-                        fontSize: 13,
-                        color: Color.fromARGB(255, 255, 255, 255)),
-                  ),
-                ),
-                Transform.translate(
-                  offset: const Offset(145, 190),
-                  child: const Text(
-                    "1.600ml",
-                    style: TextStyle(
-                        fontFamily: 'assets/fonts/GlacialIndifference',
-                        fontSize: 16,
-                        color: Color.fromARGB(255, 255, 255, 255)),
-                  ),
-                ),
-              ],
-            ),
-            Transform.translate(
-              offset: const Offset(0, 20),
-              child: Container(
-                width: 290, // Largura da caixa
-                height: 220, // Altura da caixa
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 6, 0, 12), // Cor de fundo
-                  borderRadius:
-                      BorderRadius.circular(20), // Bordas arredondadas
-                  boxShadow: [
-                    BoxShadow(
-                      color: const Color.fromRGBO(132, 0, 255, 1)
-                          .withOpacity(0.5), // Cor da sombra
-                      spreadRadius: 5, // Extensão da sombra
-                      blurRadius: 0, // Suavidade da sombra
-                      offset: const Offset(0, 0), // Posição da sombra
-                    ),
-                  ],
-                ),
-                child: const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      children: [
-                        SizedBox(width: 10), // Espaçamento no início
+            _buildHeader(),
+            _buildWaterIntakeBox(),
+            _buildBottomContainer(),
+          ],
+        ),
+      ),
+    );
+  }
 
-                        Text(
-                          "8:00",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(width: 160), 
-                        Text(
-                          "400ml",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(width: 10), 
-                      ],
-                    ),
-                    SizedBox(height: 20), // Espaçamento entre as Rows
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        SizedBox(width: 10),
-                        Text(
-                          "12:00",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(width: 160),
-                        Text(
-                          "400ml",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(width: 10), // Espaçamento no início
-                      ],
-                    ),
-                    SizedBox(height: 20), // Espaçamento entre as Rows
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        SizedBox(width: 10),
-                        Text(
-                          "16:00",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(width: 160),
-                        Text(
-                          "400ml",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(width: 10), // Espaçamento no início
-                      ],
-                    ),
-                    SizedBox(height: 20), // Espaçamento entre as Rows
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        SizedBox(width: 10),
-                        Text(
-                          "20:00",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(width: 160),
-                        Text(
-                          "400ml",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(width: 10), // Espaçamento no início
-                      ],
-                    ),
-                    SizedBox(height: 20), // Espaçamento entre as Rows
-                  ],
-                ),
-              ),
-            ),
-            Transform.translate(
-              offset: const Offset(0, 34),
-              child: Container(
-                width: 400, // Largura da caixa
-                height: 40, // Altura da caixa
-                decoration: const BoxDecoration(
-                  color: Color.fromRGBO(132, 0, 255, 1), // Cor de fundo
-                ),
-              ),
+  Widget _buildHeader() {
+    return Stack(
+      children: [
+        const Spacer(),
+        Transform.translate(
+          offset: const Offset(90, -20),
+          child: Image.asset(
+            'assets/Ze_gotinha_da_agua.png',
+            height: 140,
+          ),
+        ),
+        Transform.translate(
+          offset: const Offset(-90, -10),
+          child: Image.asset(
+            'assets/VitaValoreLogo.jpeg',
+            height: 60,
+          ),
+        ),
+        _buildHeaderText("1h26min", const Offset(105, 45), 15),
+        _buildHeaderText("800ml", const Offset(-35, 190), 13),
+        _buildHeaderText("1.600ml", const Offset(145, 190), 16),
+      ],
+    );
+  }
+
+  Widget _buildHeaderText(String text, Offset offset, double fontSize) {
+    return Transform.translate(
+      offset: offset,
+      child: Text(
+        text,
+        style: TextStyle(
+          fontFamily: 'assets/fonts/GlacialIndifference',
+          fontSize: fontSize,
+          color: const Color.fromARGB(255, 255, 255, 255),
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWaterIntakeBox() {
+    return Transform.translate(
+      offset: const Offset(0, 20),
+      child: Container(
+        width: 290,
+        height: 220,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 6, 0, 12),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: const Color.fromRGBO(132, 0, 255, 1).withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 0,
+              offset: const Offset(0, 0),
             ),
           ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildWaterIntakeRow("8:00", "400ml"),
+            const SizedBox(height: 20),
+            _buildWaterIntakeRow("12:00", "400ml"),
+            const SizedBox(height: 20),
+            _buildWaterIntakeRow("16:00", "400ml"),
+            const SizedBox(height: 20),
+            _buildWaterIntakeRow("20:00", "400ml"),
+          ],
+        ),
+      ),
+    );
+  }
+
+  static Widget _buildWaterIntakeRow(String time, String amount) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const SizedBox(width: 10),
+        Text(
+          time,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const Spacer(),
+        Text(
+          amount,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(width: 10),
+      ],
+    );
+  }
+
+  Widget _buildBottomContainer() {
+    return Transform.translate(
+      offset: const Offset(0, 34),
+      child: Container(
+        width: 400,
+        height: 40,
+        decoration: const BoxDecoration(
+          color: Color.fromRGBO(132, 0, 255, 1),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:vita_valore/login_sign.dart';
+import 'package:vita_valore/agua.dart';
+//import 'package:vita_valore/login_sign.dart';
 
 void main() {
   runApp(const MaterialApp(
     title: 'Vita Valore',
     debugShowCheckedModeBanner: false,
-    home: LoginSing(),
+    home: AguaPage(),
   ));
 }
